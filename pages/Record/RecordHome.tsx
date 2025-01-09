c3bda04e78f7739e59f4daf1bce5677269ad60de
M2 <m2@gimminbeoms-MacBook-Pro.local>
2025-01-09 18:51:57 +0900

    Feat. 초기 라우팅, 레이아웃 설정 초기 레이아웃입니다. 추후에 수정하기 편하게 root-layout 배경 설정해두었고, 라우팅 설정도 했습니다

---
+++ pages/Record/RecordHome.tsx
@@ -0,0 +1,9 @@
+const RecordHome = () => {
+  return (
+    <div>
+      <h1>record</h1>
+    </div>
+  )
+}
+
+export default RecordHome
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from '../layout/root-layout'
+import RecordHome from '../pages/Record/RecordHome'
+import Main from '../pages/Main/Main'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <div className="bg-blue-50" />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: 'record',
+        element: <RecordHome />,
+      },
+    ],
+  },
+])
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">BULK front</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
