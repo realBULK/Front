0feff2a993549f1cfd82c39d60ae433406042dfb
jelee <jelee2204@gmail.com>
2025-01-11 00:17:49 +0900

    FEAT: 질문 페이지 2-2 추가

--- src/pages/Question/Question2_2.tsx
+++ src/pages/Question/Question2_2.tsx
@@ -21,34 +21,42 @@ const Question1_2 = () => {
         평소 즐겨 먹는 음식을 알려주세요.
       </div>
 
-      <div className="flex flex-col gap-[2.35vh] w-full">
-        <div className="mb-[1.5vh]">
-          <div className="text-[#191919] font-semibold mb-[1vh]">채소</div>
-          <div className="flex flex-wrap gap-[10px]">
+      <div className="flex flex-col gap-[2.93vh] w-full">
+        <div>
+          <div className="flex w-[50px] h-[23px] flex-col justify-center flex-shrink-0 text-[#191919] text-center font-pretendard text-[24px] font-extrabold leading-[100%] tracking-[-0.48px] mb-[1.17vh]">
+            채소
+          </div>
+          <div className="flex flex-wrap gap-[5px]">
             <QuestionSmallButton text="브로콜리" />
             <QuestionSmallButton text="콜리플라워" />
             <QuestionSmallButton text="양파" />
             <QuestionSmallButton text="피망" />
             <QuestionSmallButton text="가지" />
+            <QuestionSmallButton text="양배추" />
+            <QuestionSmallButton text="시금치" />
             <QuestionSmallButton text="오이" />
             <QuestionSmallButton text="토마토" />
             <QuestionSmallButton text="아스파라거스" />
           </div>
         </div>
 
-        <div className="mb-[1.5vh]">
-          <div className="text-[#191919] font-semibold mb-[1vh]">곡물</div>
-          <div className="flex flex-wrap gap-[10px]">
+        <div>
+          <div className="flex w-[50px] h-[23px] flex-col justify-center flex-shrink-0 text-[#191919] text-center font-pretendard text-[24px] font-extrabold leading-[100%] tracking-[-0.48px] mb-[1.17vh]">
+            곡물
+          </div>
+          <div className="flex flex-wrap gap-[5px]">
             <QuestionSmallButton text="쌀" />
             <QuestionSmallButton text="퀴노아" />
             <QuestionSmallButton text="메밀" />
-            <QuestionSmallButton text="옥수수 가루" />
+            <QuestionSmallButton text="옥수숫가루" />
           </div>
         </div>
 
-        <div className="mb-[1.5vh]">
-          <div className="text-[#191919] font-semibold mb-[1vh]">재료</div>
-          <div className="flex flex-wrap gap-[10px]">
+        <div>
+          <div className="flex w-[50px] h-[23px] flex-col justify-center flex-shrink-0 text-[#191919] text-center font-pretendard text-[24px] font-extrabold leading-[100%] tracking-[-0.48px] mb-[1.17vh]">
+            재료
+          </div>
+          <div className="flex flex-wrap gap-[5px]">
             <QuestionSmallButton text="아보카도" />
             <QuestionSmallButton text="콩" />
             <QuestionSmallButton text="우유" />
@@ -57,6 +65,38 @@ const Question1_2 = () => {
             <QuestionSmallButton text="단백질 파우더" />
             <QuestionSmallButton text="두부" />
             <QuestionSmallButton text="버섯" />
+            <QuestionSmallButton text="식물성 우유" />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex w-[127px] h-[23px] flex-col justify-center flex-shrink-0 text-[#191919] text-left font-pretendard text-[24px] font-extrabold leading-[100%] tracking-[-0.48px] mb-[1.17vh]">
+            육류&생선
+          </div>
+          <div className="flex flex-wrap gap-[5px]">
+            <QuestionSmallButton text="칠면조" />
+            <QuestionSmallButton text="소고기" />
+            <QuestionSmallButton text="닭고기" />
+            <QuestionSmallButton text="해산물" />
+            <QuestionSmallButton text="돼지고기" />
+          </div>
+        </div>
+
+        <div className="mb-[50px]">
+          <div className="flex w-[200px] h-[23px] flex-col justify-center flex-shrink-0 text-[#191919] text-left font-pretendard text-[24px] font-extrabold leading-[100%] tracking-[-0.48px] mb-[1.17vh]">
+            과일 & 베리류
+          </div>
+          <div className="flex flex-wrap gap-[5px]">
+            <QuestionSmallButton text="감귤류" />
+            <QuestionSmallButton text="사과" />
+            <QuestionSmallButton text="바나나" />
+            <QuestionSmallButton text="배" />
+            <QuestionSmallButton text="키위" />
+            <QuestionSmallButton text="감" />
+            <QuestionSmallButton text="복숭아" />
+            <QuestionSmallButton text="베리류" />
+            <QuestionSmallButton text="포도" />
+            <QuestionSmallButton text="석류" />
           </div>
         </div>
       </div>
--- src/pages/Question/QuestionSmallButton.tsx
+++ src/pages/Question/QuestionSmallButton.tsx
@@ -4,7 +4,7 @@ interface QuestionSmallButtonProps {
 
 const QuestionSmallButton: React.FC<QuestionSmallButtonProps> = ({ text }) => {
   return (
-    <button className="flex h-[38px] px-[19px] py-[12px] justify-center items-center gap-[10px] rounded-[15px] border border-[#EDEDED] bg-[rgba(255,255,255,0.8)] shadow-[0px_2px_5px_-2px_rgba(0,0,0,0.25)] transition hover:bg-[#f0f4ff]">
+    <button className="flex h-[38px] px-[14px] py-[12px] justify-center items-center gap-x-[5px] gap-y-[7px] rounded-[15px] border border-[#EDEDED] bg-[rgba(255,255,255,0.8)] shadow-[0px_2px_5px_-2px_rgba(0,0,0,0.25)] transition hover:bg-[#f0f4ff]">
       {text}
     </button>
   )
