c179359e2a459ecfb8bd2060400985bda640ad42
jelee <jelee2204@gmail.com>
2025-01-20 17:06:32 +0900

    [FEAT] input 형식 질문페이지 유효성검사 예외처리

--- src/components/QuestionSmallButton.tsx
+++ src/components/QuestionSmallButton.tsx
@@ -6,7 +6,7 @@ interface QuestionSmallButtonProps {
   onSelectionChange?: (item: string, isNowSelected: boolean) => void
 }
 
-const QuestionSmallButton: React.FC<QuestionSmallButtonProps> = ({ text, selected = false, onSelectionChange }) => {
+const QuestionSmallButton: React.FC<QuestionSmallButtonProps> = ({ text, selected = true, onSelectionChange }) => {
   const [isSelected, setIsSelected] = useState(selected)
 
   const handleClick = () => {
@@ -24,7 +24,7 @@ const QuestionSmallButton: React.FC<QuestionSmallButtonProps> = ({ text, selecte
     <button
       onClick={handleClick}
       className={`flex h-[38px] px-[14px] py-[12px] justify-center items-center gap-x-[5px] gap-y-[7px] rounded-[15px] border 
-        ${isSelected ? 'bg-[#DAE6CB]' : 'bg-[rgba(255,255,255,0.8)]'} 
+        ${!isSelected ? 'bg-[#DAE6CB]' : 'bg-[rgba(255,255,255,0.8)]'} 
          shadow-[0px_2px_5px_-2px_rgba(0,0,0,0.25)] transition-colors 
         hover:bg-[#DAE6CB] outline-none`}
     >
