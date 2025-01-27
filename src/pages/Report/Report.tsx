ecd8c9c7b55545e8f356f68b4fc76a439dec4ea3
Eunjin Oh <ibsuny3214@naver.com>
2025-01-27 16:36:10 +0900

    [FEAT] 버튼 제작 (#36)

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -1,7 +1,9 @@
 import WeightProgressChart from '@/pages/Report/ReportGraph/graph'
 import NutrientsBox from '@/pages/Report/ReportGraph/nutrient'
+import { useNavigate } from 'react-router-dom'
 
 const Report: React.FC = () => {
+  const navigate = useNavigate()
   return (
     <div className="flex justify-center items-center flex-col h-screen pt-10 pb-10 pr-8 pl-8">
       <div className="text-[#191919] text-[32px] font-medium leading-[125%] tracking-[-0.64px] font-[GmarketSansWeight]">
@@ -14,6 +16,15 @@ const Report: React.FC = () => {
         하루 영양 섭취 가이드
       </div>
       <NutrientsBox />
+      <div className="w-[327px]">
+        <button
+          className="w-[327px] h-[55px] font-[pretendard] bg-[#D1D1D1] 
+      shadow-whiteBox rounded-base font-semibold text-[14px] text-[#191919] outline-none mb-9"
+          onClick={() => navigate('/suggestion')}
+        >
+          다음
+        </button>
+      </div>
     </div>
   )
 }
