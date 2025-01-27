f026b1354a2839cf3c07c97f146cc7c6da1b03da
Eunjin Oh <ibsuny3214@naver.com>
2025-01-27 15:56:37 +0900

    [FEAT] 그래프 제작 (#36)

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -1,7 +1,14 @@
+import WeightProgressChart from '@/pages/Report/ReportGraph/graph'
+
 const Report: React.FC = () => {
   return (
-    <div>
-      <h1>Report</h1>
+    <div className="flex justify-center items-center flex-col h-screen pt-10 pb-10 pr-8 pl-8">
+      <div className="text-[#191919] text-[32px] not-italic font-medium leading-[125%] tracking-[-0.64px] font-[GmarketSansWeight]">
+        <span className="text-[#8BBEB9]">균형 잡힌 식단</span>
+        <span>으로</span>
+        <p>목표 체중 달성하기</p>
+      </div>
+      <WeightProgressChart />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         whiteBox: '0px 2px 5px -2px rgba(0,0,0,0.25)',
         whiteBoxDeepShadow: '0px 6px 5px -2px rgba(0, 0, 0, 0.25)',
         blueBox: '0px 0px 20px 2px #EDEFFE inset, 0px 2px 5px -2px rgba(0, 0, 0, 0.25)',
+        graphLine: '0px 0px 2px 0px rgba(120,120,120,0.25)',
       },
       borderRadius: {
         base: '0.938rem',
