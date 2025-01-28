2035ade01926f44e8de7b885d8f8f56b717feabd
Eunjin Oh <ibsuny3214@naver.com>
2025-01-29 03:00:57 +0900

    [FEAT] handeler 변경 (#76)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,29 @@
 // src/mocks/handlers.js
-import { menu } from '@/mocks/apis/menu'
+import { menu, menuChange, menuDetail } from '@/mocks/apis/menu'
+import { home } from '@/mocks/apis/home'
+import { groupEmoji, groupEmojiDelete, groupEmojiPatch, groupLank, groupList, groupMap } from '@/mocks/apis/group'
+import { userData, userDuplicate, userReport, userReportPatch } from '@/mocks/apis/question'
+import { recordData } from '@/mocks/apis/record'
+
 import { delay, http } from 'msw'
 
 export const handlers = [
   http.all('*', async () => {
     await delay(100)
   }),
-  menu, // 추가된 핸들러
+  menu,
+  menuChange,
+  menuDetail,
+  home,
+  groupEmoji,
+  groupEmojiDelete,
+  groupEmojiPatch,
+  groupLank,
+  groupList,
+  groupMap,
+  userData,
+  userDuplicate,
+  userReport,
+  userReportPatch,
+  recordData,
 ]
