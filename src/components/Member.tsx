099029897d8c7c6a1e00ca2903eee18f347b1d94
phjlia2430 <phjlia2430@naver.com>
2025-01-29 22:58:24 +0900

    [FEAT] 오늘의 팀원(#82)

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -6,28 +6,28 @@ import PressingHeart from '../assets/PressingHeart.svg';
 import { useState } from 'react';
 
 const Member = () => {
-  // 각 하트의 상태와 클릭 수를 관리
-  const [likes, setLikes] = useState<number[]>(Array(8).fill(0)); // 초기 0으로 설정
+  const length = 10; // 팀원 수 나타내는 변수
+  const [likes, setLikes] = useState<number[]>(Array(length).fill(0)); 
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
 
   const handleHeartClick = (index: number) => {
     setLikes((prevLikes) =>
       prevLikes.map((like, i) =>
-        i === index ? (like > 0 ? 0 : 1) : like // 눌린 상태 토글 (0 또는 1)
+        i === index ? (like > 0 ? 0 : 1) : like // 눌린 상태 토글 
       )
     );
   };
 
   return (
-    <div className="bg-[#F5F5F5] min-h-screen flex flex-col items-center pt-[452px] mb-12">
+    <div className="bg-[#F5F5F5] min-h-screen flex flex-col items-center pt-[50px] mb-[80px]">
       <div className="w-[327px]">
         <h2 className="text-[24px] font-bold text-[#000000] font-[pretendard] ml-2 text-left">오늘의 팀원</h2>
       </div>
       <Box
-        className="w-[327px] h-auto grid grid-cols-5 gap-x-2 bg-white/80 rounded-base border border-[#EDEDED] shadow-whiteBoxDeepShadow p-4"
+        className="w-[327px] h-[226px] grid grid-cols-5 gap-x-2 bg-white/80 rounded-base border border-[#EDEDED] shadow-whiteBoxDeepShadow p-4"
       >
         {/* 팀원 리스트 */}
-        {Array.from({ length: 8 }).map((_, index) => (
+        {Array.from({ length }).map((_, index) => (
           <div
             key={index}
             className="flex flex-col items-center w-[60px] h-[100px]"
@@ -40,7 +40,7 @@ const Member = () => {
                 className="w-full h-full object-cover"
               />
             </div>
-            <p className="text-[7px] font-[pretendard] text-[#000000] mt-1">닉네임</p>
+            <p className="text-[8px] font-[pretendard] text-[#000000] mt-1">abaegopa12</p>
             <div
               className="flex items-center mt-1"
               onMouseEnter={() => setHoverIndex(index)}
