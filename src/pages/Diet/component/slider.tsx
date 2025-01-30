7ec9c517b035a01424347041fa402e0c3b8c848e
Eunjin Oh <ibsuny3214@naver.com>
2025-01-30 23:42:30 +0900

    [FEAT] slider 제작완료 (react-slider 라이브러리 생성)  (#80)

--- src/pages/Diet/component/slider.tsx
+++ src/pages/Diet/component/slider.tsx
@@ -1,13 +1,29 @@
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import React from 'react'
+import DietBox from '@/components/DietBox'
 
-const DietSlider = () => {
+interface DietSliderProps {
+  id: string
+  title: string
+  items: string[]
+  nutrients: string[]
+  Kcal: number
+  icon?: string
+  isDetail?: boolean
+}
+
+interface Diet {
+  dietItem: DietSliderProps[]
+}
+
+const DietSlider: React.FC<Diet> = ({ dietItem }) => {
   const settings = {
     dots: true,
     arrows: false,
     fade: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -15,25 +31,51 @@ const DietSlider = () => {
   }
 
   return (
-    <div className="carousel">
-      <Slider {...settings}>
+    <div className="mt-4">
+      <Slider {...settings} dotsClass="test-css">
         <div>
-          <h3>1djfskjlskjjsfljksflsjfskljf</h3>
+          <DietBox
+            id={dietItem[0].id}
+            title={dietItem[0].title}
+            items={dietItem[0].items}
+            nutrients={dietItem[0].nutrients}
+            Kcal={dietItem[0].Kcal}
+            icon={dietItem[0].icon}
+            isDetail={false}
+          />
         </div>
         <div>
-          <h3>2kfdjsfljaslfjasljkf</h3>
+          <DietBox
+            id={dietItem[1].id}
+            title={dietItem[1].title}
+            items={dietItem[1].items}
+            nutrients={dietItem[1].nutrients}
+            Kcal={dietItem[1].Kcal}
+            icon={dietItem[1].icon}
+            isDetail={false}
+          />
         </div>
         <div>
-          <h3>3sflasdfsfjslfk</h3>
+          <DietBox
+            id={dietItem[2].id}
+            title={dietItem[2].title}
+            items={dietItem[2].items}
+            nutrients={dietItem[2].nutrients}
+            Kcal={dietItem[2].Kcal}
+            icon={dietItem[2].icon}
+            isDetail={false}
+          />
         </div>
         <div>
-          <h3>4fdfjskdjkjfkedf</h3>
-        </div>
-        <div>
-          <h3>5fjaksjlajflk</h3>
-        </div>
-        <div>
-          <h3>6dkfjlfjaemjawfmwkm</h3>
+          <DietBox
+            id={dietItem[3].id}
+            title={dietItem[3].title}
+            items={dietItem[3].items}
+            nutrients={dietItem[3].nutrients}
+            Kcal={dietItem[3].Kcal}
+            icon={dietItem[3].icon}
+            isDetail={false}
+          />
         </div>
       </Slider>
     </div>
