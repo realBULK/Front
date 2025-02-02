cddec55d0663c7f7c808647af89c37d776cd4525
Eunjin Oh <ibsuny3214@naver.com>
2025-02-03 02:50:44 +0900

    [REFACTO] 기존 dietMenu fetch 함수와 파일 분리 (#97)

--- src/hooks/useDietMenu.ts
+++ src/hooks/useDietMenu.ts
@@ -1,15 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
-const fetchDietMenu = async () => {
-  try {
-    const response = await axios.get('/api/menu')
-    return response.data
-  } catch (error) {
-    throw new Error('데이터를 불러오는 데 실패했습니다.')
-  }
-}
-
 // 특정 날짜에 해당하는 dailyMeals만 필터링
 const fetchDietMenuDaily = async (date: string) => {
   try {
@@ -34,13 +25,6 @@ const fetchDietMenuDaily = async (date: string) => {
   } catch (error) {
     throw new Error('데이터를 불러오는 데 실패했습니다.')
   }
-}
-
-export const useDietMenu = () => {
-  return useQuery({
-    queryKey: ['userDietMenu'],
-    queryFn: fetchDietMenu,
-  })
 }
 
 export const useDietMenuDaily = (date: string) => {
