6316f34809bea839085fc24ac09b0d2f8731da67
heeone1 <rinakim1019@gmail.com>
2025-02-04 00:37:13 +0900

    [FEAT] 메인화면 컴포넌트 분리

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect} from "react";
 import NavBar from "../../components/NavBar";
 import { useNavigate } from "react-router-dom";
 
