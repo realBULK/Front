0b957fc68e8030ff7cd41dee1b81745d36fd0b67
heeone1 <rinakim1019@gmail.com>
2025-02-05 02:58:12 +0900

    [Design] 메인 화면 반응형 수정

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -28,17 +28,17 @@ const Start = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center">
-        <img src={logo} alt="Logo" className="w-[252px] h-[135px] mb-[75px] mt-[15%]" />
+        <img src={logo} alt="Logo" className="w-[252px] h-[135px] mb-[36px] mt-[15%]" />
         <BigWhiteButton text="맞춤 식단 받아보기" navigateTo="questionstart"></BigWhiteButton>
         <div className="flex items-center w-[327px]">
-          <hr className="flex-1 border-[#535353]" />
-          <span className="mx-4 text-[14px] text-[#535353]">또는</span>
-          <hr className="flex-1 border-[#535353]" />
+          <hr className="flex-1 border-[#A4A4A4]" />
+          <span className="mx-4 text-[14px] text-[#A4A4A4]">또는</span>
+          <hr className="flex-1 border-[#A4A4A4]" />
         </div>
 
         <div className="mt-[35px] flex flex-col gap-2 w-full">
           <button
-            className="w-[327px] h-[57px] text-[16px] font-[Pretendard] font-semibold text-[#000000] text-center rounded-[200px] bg-[#FAE100] active:bg-[#998C17] flex items-center justify-center gap-1 hover:bg-[#ffd900]"
+            className="w-[327px] h-[57px] text-[16px] font-[Pretendard] font-semibold text-[#000000] text-center rounded-[200px] bg-[#FAE100] active:bg-[#998C17] flex items-center justify-center gap-1"
             style={{
               border: '1px solid #FAE100',
             }}
@@ -49,7 +49,7 @@ const Start = () => {
           </button>
 
           <button
-            className="w-[327px] h-[57px] text-[16px] font-[Pretendard] font-semibold text-[#FFFFFF] text-center rounded-[200px] bg-[#000000] flex items-center justify-center gap-1 hover:bg-[#212121]"
+            className="w-[327px] h-[57px] text-[16px] font-[Pretendard] font-semibold text-[#FFFFFF] text-center rounded-[200px] bg-[#000000] flex items-center justify-center gap-1"
             style={{
               border: '1px solid #000000',
             }}
