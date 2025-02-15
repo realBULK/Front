2212224c38c8c3640259f93fd7fd6162b059beb7
Eunjin Oh <ibsuny3214@naver.com>
2025-02-16 03:59:22 +0900

    [FEAT] hooks 변경 (#111)

--- src/hooks/useCheckNickname.ts
+++ src/hooks/useCheckNickname.ts
@@ -1,34 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/apis/axiosInstance'
 
 type CheckNicknameResponse = {
-  isSuccess: boolean;
-  code: string;
-  message: string;
-  status: string;
+  isSuccess: boolean
+  code: string
+  message: string
+  status: string
   data: {
-    duplicated: boolean;
-  };
-};
+    duplicated: boolean
+  }
+}
 
 export const useCheckNickname = (nickname: string) => {
   return useQuery<CheckNicknameResponse>({
-    queryKey: ["checkNickname", nickname || ""], // 안전하게 queryKey 설정
+    queryKey: ['checkNickname', nickname || ''], // 안전하게 queryKey 설정
     queryFn: async () => {
       if (!nickname.trim()) {
         // 닉네임이 없으면 빈 데이터 반환
         return {
           isSuccess: false,
-          code: "400",
-          message: "닉네임이 입력되지 않았습니다.",
-          status: "BAD_REQUEST",
+          code: '400',
+          message: '닉네임이 입력되지 않았습니다.',
+          status: 'BAD_REQUEST',
           data: { duplicated: false },
-        };
+        }
       }
-      const response = await axios.get(`/api/user/question/isDuplicated/${nickname}`);
-      return response.data;
+      const response = await api.get(`/api/user/question/isDuplicated/${nickname}`)
+      return response.data
     },
     enabled: nickname.trim().length > 0, // 닉네임이 입력된 경우에만 실행
     retry: false, // 실패 시 재시도 방지
-  });
-};
+  })
+}
