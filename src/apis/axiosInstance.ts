14055d7f0721dded112d1327d69d9f2d34082686
Eunjin Oh <ibsuny3214@naver.com>
2025-02-16 17:30:22 +0900

    [FIX] axios에 사용자 토큰 코드 추가 (#111)

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -2,9 +2,19 @@ import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_BASEURL
 
-export const api = axios.create({
+const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
   baseURL: baseURL,
 })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default api
